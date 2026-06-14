f5cd0e3cdc9aab157bfb2eef97ebdd48124e1381
MADHAV PATIDAR <madhavpatidar@MADHAVs-MacBook-Air.local>
2026-06-14 08:33:12 +0530

    Set Login Page Default

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Verify from "./pages/Verify";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Certificate from "./pages/Certificate";
@@ -8,25 +7,14 @@ import Certificate from "./pages/Certificate";
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
-
         <Route path="/" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
-
         <Route path="/dashboard" element={<Dashboard />} />
-
         <Route path="/certificate" element={<Certificate />} />
-
       </Routes>
-
     </BrowserRouter>
   );
 }
-<Route
-  path="/verify/:id"
-  element={<Verify />}
-/>
 
 export default App;
